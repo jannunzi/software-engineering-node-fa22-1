6c000309a187e8b741e225279cbb982955fba607
JoseAnnunziatoSonos <Jose.Annunziato@sonos.com>
2022-09-30 09:35:33 -0400

    h1

--- users/User.ts
+++ users/User.ts
@@ -3,6 +3,7 @@ import MaritalStatus from "./MaritalStatus";
 import Location from "./Location";
 
 export default class User {
+    private id: string;
     private username: string = '';
     private password: string = '';
     private firstName: string | null = null;
@@ -16,10 +17,33 @@ export default class User {
     private dateOfBirth: Date | null = null;
     private joined: Date = new Date();
     private location: Location | null = null;
-    constructor(username: string, password: string) {
+    constructor(id: string, username: string, password: string, firstName: string, lastName: string) {
+        this.id = id;
         this.username = username;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
     get uName() { return this.username; }
     get pass() { return this.password; }
+    get fName() { return this.firstName; }
+    get lName() { return this.lastName; }
+    get json() {
+        return {
+            id: this.id,
+            username: this.username,
+            password: this.password,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email,
+            profilePhoto: this.profilePhoto,
+            headerImage: this.headerImage,
+            accountType: this.accountType,
+            maritalStatus: this.maritalStatus,
+            biography: this.biography,
+            dob: this.dateOfBirth,
+            joined: this.joined,
+            location: this.location
+        }
+    }
 }
--- users/UserDao.ts
+++ users/UserDao.ts
@@ -10,8 +10,11 @@ export default class UserDao implements UserDaoI {
 
         const userModels = userMongooseModels.map((userMongooseModel) => {
             return new User(
-                userMongooseModel.username,
-                userMongooseModel.password
+                userMongooseModel?._id.toString()??'',
+                userMongooseModel?.username??'',
+                userMongooseModel?.password??'',
+                userMongooseModel?.firstName??'',
+                userMongooseModel?.lastName??'',
             );
         });
         return userModels;
@@ -19,24 +22,28 @@ export default class UserDao implements UserDaoI {
     async findUserById(uid: string): Promise<User> {
         const userMongooseModel = await UserModel.findById(uid);
         return new User(
+            userMongooseModel?._id.toString()??'',
             userMongooseModel?.username??'',
-            userMongooseModel?.password??''
+            userMongooseModel?.password??'',
+            userMongooseModel?.firstName??'',
+            userMongooseModel?.lastName??'',
         );
     }
     async createUser(user: User): Promise<User> {
         const userMongooseModel = await UserModel.create(user);
         return new User(
-            userMongooseModel.username,
-            userMongooseModel.password
+            userMongooseModel?._id.toString()??'',
+            userMongooseModel?.username??'',
+            userMongooseModel?.password??'',
+            userMongooseModel?.firstName??'',
+            userMongooseModel?.lastName??'',
         );
     }
     async deleteUser(uid: string):  Promise<any> {
         return await UserModel.deleteOne({_id: uid});
     }
-    async updateUser(uid: string, user: User): Promise<any> {
-        return await UserModel.updateOne({_id: uid}, {$set: {
-                username: user.uName,
-                password: user.pass
-            }});
+    async updateUser(uid: string, user: any): Promise<any> {
+        // console.log(uid, user)
+        return await UserModel.updateOne({_id: uid}, {$set: user});
     }
 }
