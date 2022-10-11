f8959091120aae3ac6755723b2ee87a59a4ab1bc
JoseAnnunziatoSonos <Jose.Annunziato@sonos.com>
2022-10-10 22:38:37 -0400

    tests

--- jest/generations.test.ts
+++ jest/generations.test.ts
@@ -12,7 +12,7 @@ const TESTS = [
     {yob: 1964, expected: 'BOOMER'},
     {yob: 1965, expected: 'X'},        {yob: 1970, expected: 'X'},
     {yob: 1980, expected: 'X'},
-    {yob: 1981, expected: 'MILLENIAL'},{yob: 1985, expected: 'MILLENIAL'},
+    {yob: 1981, expected: 'Y'},{yob: 1985, expected: 'MILLENIAL'},
     {yob: 1996, expected: 'MILLENIAL'},
     {yob: 1997, expected: 'Z'},        {yob: 2000, expected: 'Z'},
     {yob: 2012, expected: 'Z'},
@@ -23,7 +23,7 @@ const TESTS = [
 // test('Someone born before 1883 is unknown generation', () => {
 //     expect(generation(1880)).toBe('UNKNOWN');
 // });
-//
+// //
 // test('Someone born in 1883 is from the LOST generation', () => {
 //     expect(generation(1883)).toBe('LOST');
 // });
